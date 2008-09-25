8249f5b9e1cb9772e852e7917293d7a8ac8ac2c7
S. Brent Faulkner <brentf@unwwwired.net>
2008-09-25 16:16:02 -0400

    initial commit

---
+++ README.markdown
@@ -0,0 +1,19 @@
+# has\_permalink
+
+Easily add permalink support to a model.
+
+## Example
+
+    # models/post.rb
+    class Post < ActiveRecord::Base
+      has_permalink :title
+    end
+
+## Installation
+
+    $ script/plugin install git://github.com/sbfaulkner/has_permalink.git
+
+## Legal
+
+**Author:** S. Brent Faulkner <brentf@unwwwired.net>  
+**License:** Copyright &copy; 2008 unwwwired.net, released under the MIT license
---
+++ lib/has_permalink.rb
@@ -0,0 +1,28 @@
+module HasPermalink
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def has_permalink(*attrs)
+      options = attrs.extract_options!
+
+      param_attr = options[:param] || attrs.first
+      param_attr = param_attr.to_s if param_attr
+
+      attrs.each do |attr|
+        title_attr = attr.to_s
+        link_attr = "#{attr}_permalink"
+
+        before_validation "update_#{link_attr}"
+
+        validates_presence_of title_attr, link_attr
+        validates_format_of link_attr, :with => /^\w+$/
+        validates_uniqueness_of link_attr
+
+        self.class_eval "def update_#{link_attr};self.#{link_attr}=#{title_attr} if #{link_attr}.blank?;self.#{link_attr}=#{link_attr}.gsub(/\\W+/,'-').gsub(/^-|-$/,'').downcase;end"
+        self.class_eval "def to_param;#{link_attr};end" if title_attr == param_attr
+      end
+    end
+  end
+end
