61eb0e9727a2a86b47e7acd1230f7f9badbf80dc
S. Brent Faulkner <brentf@unwwwired.net>
2008-09-25 16:49:41 -0400

    :param attribute need to be handled differently since false/nil is a possible value

--- lib/has_permalink.rb
+++ lib/has_permalink.rb
@@ -7,7 +7,7 @@ module HasPermalink
     def has_permalink(*attrs)
       options = attrs.extract_options!
 
-      param_attr = options[:param] || attrs.first
+      param_attr = options.include?(:param) ? options[:param] : attrs.first
       param_attr = param_attr.to_s if param_attr
 
       attrs.each do |attr|
@@ -20,8 +20,8 @@ module HasPermalink
         validates_format_of link_attr, :with => /^[A-Za-z0-9_-]+$/
         validates_uniqueness_of link_attr
 
-        self.class_eval "def update_#{link_attr};self.#{link_attr}=#{title_attr} if #{link_attr}.blank?;self.#{link_attr}=#{link_attr}.gsub(/[^A-Za-z0-9_-]+/,'-').gsub(/^-|-$/,'').downcase;end"
-        self.class_eval "def to_param;#{link_attr};end" if title_attr == param_attr
+        self.class_eval("def update_#{link_attr};self.#{link_attr}=#{title_attr} if #{link_attr}.blank?;self.#{link_attr}=#{link_attr}.gsub(/[^A-Za-z0-9_-]+/,'-').gsub(/^-|-$/,'').downcase;end")
+        self.class_eval("def to_param;#{link_attr};end") if title_attr == param_attr
       end
     end
   end
